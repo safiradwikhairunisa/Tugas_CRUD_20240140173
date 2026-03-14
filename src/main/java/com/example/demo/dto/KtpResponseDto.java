23699566d0fee109e39901ea5c79c6dbaa027226
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2026-03-14 23:07:16 +0700

    add Ktp response

---
+++ src/main/java/com/example/demo/dto/KtpResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KtpResponseDto {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
