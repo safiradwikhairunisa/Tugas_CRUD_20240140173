3e565319c0259d5d824e9cbc5a49beeeb340afb8
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2026-03-14 22:23:11 +0700

    add ktp model

---
+++ src/main/java/com/example/demo/model/Ktpmodel.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ktpmodel {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
